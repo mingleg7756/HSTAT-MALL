537b51d934a6bb718de1a3dfb1fe5958f5b7300b
Ming Le Gao <mingleg7756@hstat.org>
2019-05-14 14:26:30 +0000

    change jQuery from alert to fadeOut

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 $(function(){
-    alert("All sales are final! By clicking ok, you agree to our terms and services!");
+    $("#agree").click(function(){
+        $("#agreement").fadeOut("slow");
+        $(this).fadeOut("slow");
+    });
 });
